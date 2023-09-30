b2d01514d7d1fd38a44c9e3cb10ecbb768759daa
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-09-30 07:41:48 +0100

    update cors

--- vidoes_apis/settings.py
+++ vidoes_apis/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
     'rest_framework'
     'video.apps.VideoConfig',
 ]
