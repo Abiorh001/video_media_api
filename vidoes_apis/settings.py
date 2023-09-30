0c71d51c0f73e8dd4597a70742954489fbc722d5
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-09-30 22:07:44 +0100

    update settings

--- vidoes_apis/settings.py
+++ vidoes_apis/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'vidoes_apis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'video_apidb',
-        'USER': 'postgres',
-        'PASSWORD': 'Lucifer_001',
-        'HOST': 'smartedconnectdb.cyhabgnrr90h.eu-west-3.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'video_apidb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Lucifer_001',
+#         'HOST': 'smartedconnectdb.cyhabgnrr90h.eu-west-3.rds.amazonaws.com',
+#         'PORT': '5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
