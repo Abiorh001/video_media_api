d681d00fea538c5dc16310091d0e54c1b7c539de
Ubuntu <ubuntu@ip-172-31-34-81.eu-west-3.compute.internal>
2023-10-01 17:40:52 +0000

    cors

--- video/views.py
+++ video/views.py
@@ -62,7 +62,7 @@ class CreateListVideo(APIView):
         serializer = VideoSerializer(data=data)
         if serializer.is_valid():
             video_obj = serializer.save()
-            video_url = f'https://malzahra.tech/api/videos/{video_obj.id}/'
+            video_url = f'https://zuri-stage-6-apiss.onrender.co/api/videos/{video_obj.id}/'
             video_obj.video_url = video_url
             video_obj.save()
 
